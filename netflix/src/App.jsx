2ee1cc38ec3cbc79e3ff668319ca9f894965a786
Ann <annrosemk227@gmail.com>
2025-12-08 01:19:02 +0530

    banner

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar/NavBar";
 import "./App.css";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
     </div>
   );
 }
--- netflix/src/components/Banner/Banner.jsx
+++ netflix/src/components/Banner/Banner.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import "./Banner.css";
 function Banner() {
-  return <div></div>;
+  return (
+    <div className="banner">
+      <div className="content">
+        <h1 className="title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
+        </div>
+        <h1 className="description">
+          This is the description for the movie. This is a placeholder text to
+          show how the description will look like in the banner component.
+        </h1>
+      </div>
+      <div className="fade_bottom"></div>
+    </div>
+  );
 }
 
 export default Banner;
