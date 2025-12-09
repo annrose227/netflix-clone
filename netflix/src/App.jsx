1fbfc2661c88e7136b102c802b84bdec427798d6
Ann <annrosemk227@gmail.com>
2025-12-09 23:17:35 +0530

    axios integration

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -3,14 +3,16 @@ import Navbar from "./components/Navbar/NavBar";
 import "./App.css";
 import Banner from "./components/Banner/Banner";
 import RowPost from "./components/RowPost/RowPost";
+import { originals, action, comedy } from "./urls";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
-      <RowPost title="Originals" />
-      <RowPost title="Action" isSmall />
+      <RowPost url={originals} title="Originals" />
+      <RowPost url={action} title="Action" isSmall />
+      <RowPost url={comedy} title="Comedy" isSmall />
     </div>
   );
 }
--- netflix/src/components/RowPost/RowPost.jsx
+++ netflix/src/components/RowPost/RowPost.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./RowPost.css";
 import axios from "../../axios.js";
-import { API_KEY, imageUrl } from "../../constants/constants";
+import { imageUrl, API_KEY } from "../../constants/constants";
+import Youtube from "react-youtube";
 
 function RowPost(props) {
   const [Movies, setMovies] = useState([]);
+  const [urlId, seturlId] = useState("");
   useEffect(() => {
     axios
-      .get(`discover/tv?api_key=${API_KEY}&with_networks=213`)
+      .get(props.url)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.results);
@@ -16,17 +18,42 @@ function RowPost(props) {
         alert("Network error");
       });
   }, []);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleMovieTrailer = (id) => {
+    console.log(id);
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        if (response.data.results.length !== 0) {
+          seturlId(response.data.results[0]);
+        } else {
+          console.log("Trailer not available");
+        }
+      });
+  };
+
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
         {Movies.map((obj) => (
           <img
+            key={obj.id}
+            onClick={() => handleMovieTrailer(obj.id)}
             className={props.isSmall ? "small_poster" : "poster"}
-            src={`${imageUrl + obj.backdrop_path}`}
+            src={`${imageUrl + (obj.backdrop_path || obj.poster_path)}`}
           />
         ))}
       </div>
+      {urlId && <Youtube opts={opts} videoId={urlId.key} />}
     </div>
   );
 }
